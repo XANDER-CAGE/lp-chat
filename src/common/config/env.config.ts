9ca5d9accff1ed5d8719176e1b07db5239c6c6eb
Xander <kaspergreen123@gmail.com>
2025-01-08 15:09:17 +0500

    Update env.config.ts

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -13,10 +13,6 @@ export const env = cleanEnv(process.env, {
   MINIO_SECRET_KEY: str(),
   MINIO_BUCKET: str(),
   BOT_TOKEN: str(),
-  KEYCLOAK_HOST: str(),
-  KEYCLOAK_CLIENT_ID: str(),
-  KEYCLOAK_REALM: str(),
-  KEYCLOAK_CLIENT_SECRET: str(),
   REJECTED_MESSAGE_TIMEOUT_IN_MINUTES: num({ default: 30 }),
   FIND_FREE_OPERATORS_CRON_PATTERN: str({ default: '*/1 * * * *' }),
 
